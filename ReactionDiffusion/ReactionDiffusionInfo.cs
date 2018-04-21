b0f758252abfffaeaa3d1023d83ae1e497fbd097
Abstractmachina <taole.chen@gmail.com>
2018-04-21 11:35:34 +0100

    Add project files.

---
+++ ReactionDiffusion/ReactionDiffusionInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace ReactionDiffusion
+{
+    public class ReactionDiffusionInfo : GH_AssemblyInfo
+    {
+        public override string Name {
+            get
+            {
+                return "ReactionDiffusion";
+            }
+        }
+        public override Bitmap Icon {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id {
+            get
+            {
+                return new Guid("ff5b6c24-6dcf-4879-864d-e451e9cab734");
+            }
+        }
+
+        public override string AuthorName {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
